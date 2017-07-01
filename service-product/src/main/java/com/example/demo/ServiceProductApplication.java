1754b09773e6a2bbc4c0dd950574c5d4752d21a7
nlin <nlin@thoughtworks.com>
2017-07-01 10:37:15 +0800

    initial all seed projects

--- service-product/src/main/java/com/example/demo/ServiceProductApplication.java
+++ service-product/src/main/java/com/example/demo/ServiceProductApplication.java
@@ -1,23 +1,41 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ServiceProductApplication {
 
     @RequestMapping("/categories")
     List<Category> categories() {
+
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return Arrays.asList(
                 new Category(1, "T-Shirt"),
                 new Category(2, "Mobile Phone"),
                 new Category(3, "Food"));
+    }
+
+    @Value("${service.hello}")
+    private String hello;
+
+    @RequestMapping("/helloword")
+    public String hello(){
+        return hello;
     }
 
     public static void main(String[] args) {
